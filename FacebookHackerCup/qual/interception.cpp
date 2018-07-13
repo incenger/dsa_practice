58820c62727f9f0ba2e741ab623d4a7f69f29cdb
incenger <dangtam.letan@gmail.com>
2018-07-13 23:08:15 +0700

    Edit Problem 2 Hacker Cup and remove redundant files

--- FacebookHackerCup/qual/interception.cpp
+++ FacebookHackerCup/qual/interception.cpp
@@ -18,28 +18,8 @@ int main(int argc, char const *argv[])
         for (int i = n; i >= 0; i--) {
             cin >> p[i];
         }
-        vector<int> a(n+1);
-        bool neg = false;
-        for (int  i = n; i > 0; i--) {
-            a[i] = p[i-1] + i;
-            if (a[i] < 0) neg = false;
-        }
-        a[0] = 0;
-        int firstZero = -1;
-        for (int  i = n; i >= 0; i--) {
-            if (a[i] == 0) {
-                firstZero = i;
-                break;
-            }
-        }
-        firstZero ++;
-        int dis = n - firstZero;
         cout << "Case #" << t << ": ";
-        if (neg) {
-            cout << 0;
-        } else if (dis < 0) {
-            cout << 0;
-        } else if (dis % 2 == 0) {
+        if (n % 2 == 1) {
             cout << 1 << endl;
             cout << fixed << setprecision(6) <<  0.0;
         } else {
