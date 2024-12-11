c61a0d59c07aa00571b2668a388afd19a37c863f
Tam Le <dangtam.letan@gmail.com>
2024-12-11 14:13:26 +0800

    Move the `count_stones` function to the global scope

--- aoc/2024/11.py
+++ aoc/2024/11.py
@@ -1,6 +1,6 @@
 """
 time python3 11.py
-python3 11.py  0.11s user 0.02s system 88% cpu 0.145 total
+python3 11.py  0.10s user 0.02s system 91% cpu 0.139 total
 """
 import functools
 
@@ -13,24 +13,23 @@ def read_input(file):
         return list(map(int, f.readline().split()))
 
 
+@functools.cache
+def count_stones(num, blinks):
+    """Number of stones created from this one (including itself) after blinks."""
+    if blinks == 0:
+        return 1
+    if num == 0:
+        return count_stones(1, blinks - 1)
+    digits = str(num)
+    if len(digits) % 2 == 1:
+        return count_stones(num * 2024, blinks - 1)
+    else:
+        left = int(digits[:len(digits) // 2])
+        right = int(digits[len(digits) // 2:])
+        return count_stones(left, blinks - 1) + count_stones(right, blinks - 1)
+
+
 def part_1(file):
-
-    @functools.cache
-    def count_stones(num, blinks):
-        """Number of stones created from this one (including itself) after blinks."""
-        if blinks == 0:
-            return 1
-        if num == 0:
-            return count_stones(1, blinks - 1)
-        digits = str(num)
-        if len(digits) % 2 == 1:
-            return count_stones(num * 2024, blinks - 1)
-        else:
-            left = int(digits[:len(digits) // 2])
-            right = int(digits[len(digits) // 2:])
-            return count_stones(left, blinks - 1) + count_stones(
-                right, blinks - 1)
-
     stones = read_input(file)
     BLINKS = 25
     answer = 0
@@ -40,22 +39,6 @@ def part_1(file):
 
 
 def part_2(file):
-
-    @functools.cache
-    def count_stones(num, blinks):
-        """Number of stones created from this one (including itself) after blinks."""
-        if blinks == 0:
-            return 1
-        if num == 0:
-            return count_stones(1, blinks - 1)
-        digits = str(num)
-        if len(digits) % 2 == 1:
-            return count_stones(num * 2024, blinks - 1)
-        else:
-            left = int(digits[:len(digits) // 2])
-            right = int(digits[len(digits) // 2:])
-            return count_stones(left, blinks - 1) + count_stones(
-                right, blinks - 1)
 
     stones = read_input(file)
     BLINKS = 75
