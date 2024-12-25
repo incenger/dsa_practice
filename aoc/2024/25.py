66477b8edeced2f324769dc6aef3ce1ecaeac767
Tam Le <dangtam.letan@gmail.com>
2024-12-25 13:36:54 +0800

    AOC 2024 Day 25 Part 1

---
+++ aoc/2024/25.py
@@ -0,0 +1,62 @@
+"""
+"""
+SAMPLE_FILE = "sample.txt"
+INPUT_FILE = "input.txt"
+
+
+def read_input(file):
+    semantics = [[]]
+    with open(file, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line:
+                semantics[-1].append(list(line))
+            else:
+                semantics.append([])
+    return semantics
+
+
+def height_encoding(semantic):
+    encoding = [-1] * len(semantic[0])
+    for row in semantic:
+        for col_idx, ch in enumerate(row):
+            if ch == '#':
+                encoding[col_idx] += 1
+    is_lock = semantic[0][0] == '#'
+    return tuple(encoding), is_lock
+
+
+def part_1(file):
+    semantics = read_input(file)
+    height_encodings = list(map(height_encoding, semantics))
+    lock_encodings = []
+    key_encodings = []
+    for semantic in semantics:
+        encoding, is_lock = height_encoding(semantic)
+        if is_lock:
+            lock_encodings.append(encoding)
+        else:
+            key_encodings.append(encoding)
+
+    answer = 0
+    MAX_HEIGHT = 5
+    for key_encoding in key_encodings:
+        for lock_encoding in lock_encodings:
+            match = all(key_height + lock_height <= MAX_HEIGHT
+                        for key_height, lock_height in zip(
+                            key_encoding, lock_encoding))
+            if match:
+                answer += 1
+
+    print("ANSWER", answer)
+
+
+def part_2(file):
+    pass
+
+
+if __name__ == "__main__":
+    part_1(SAMPLE_FILE)
+    part_1(INPUT_FILE)
+    # part_2(SAMPLE_FILE)
+    # part_2(INPUT_FILE)
